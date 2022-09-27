a6f3be8588a16af11c2055e1231ca0e97ebe21e8
fumiakikobayashi <vbxy95xwy@gmail.com>
2022-09-28 08:40:25 +0900

    add loading-component

--- src/frontend/pages/customers/index.tsx
+++ src/frontend/pages/customers/index.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from "next"
-import {useUserState} from "../../atoms/userAtom"
+import type {NextPage} from "next"
 import axios from "../../libs/axios"
+import {AxiosError, AxiosResponse} from "axios"
 import {useEffect, useState} from "react"
 import {NextRouter} from "next/dist/shared/lib/router/router"
 import {useRouter} from "next/router"
 import {useAuth} from "../hooks/useAuth";
+import Link from "next/link";
+import {Loading} from "../../components/Loading";
 
 type Customer = {
     title: string;
@@ -14,7 +16,8 @@ type Customer = {
 const Customer: NextPage = () => {
     const router: NextRouter = useRouter()
     const [customers, setCustomers] = useState<Customer[]>([])
-    const { checkLoggedIn } = useAuth()
+    const {checkLoggedIn} = useAuth()
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const init = async () => {
@@ -22,13 +25,26 @@ const Customer: NextPage = () => {
             if (!response) {
                 await router.push('/login')
             }
+            axios
+                .get('/api/customers')
+                .then((response: AxiosResponse) => {
+                    console.log(response.data);
+                    setCustomers(response.data.data);
+                })
+                .catch((err: AxiosError) => console.log(err.response))
+                .finally(() => setIsLoading(false));
         }
         init()
     }, [])
 
+    if (isLoading) return <Loading />
+
     return (
         <div>
-            顧客一覧
+            <Link href='/customers/post'>
+                <a>顧客を追加する</a>
+            </Link>
+            <p>顧客一覧</p>
         </div>
     )
 }
